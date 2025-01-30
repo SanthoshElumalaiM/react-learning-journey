504496d8d82b5893e1a164e8a6f5e88d1e76b562
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-30 13:58:44 +0530

    Hooks

---
+++ practice/src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <header>
+        <nav>
+            <li>Home</li>
+            <li>Abount Us</li>
+            <li>Contact Us</li>
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default Home
