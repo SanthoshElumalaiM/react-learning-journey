fd3fa1b71bcded7001089e4400d06e9392d7aa04
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-25 17:49:22 +0530

    JSX

--- firstApp/src/App.jsx
+++ firstApp/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+let App=()=>{
 
-function App() {
-  const [count, setCount] = useState(0)
+  let a=10,b=20;
+  let myName="Sant"
+  return(
+    <div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+       <img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="container">
+          <h1>{a+b}</h1>
+          <h1>{myName}</h1>
+        </div>
+    </div>
   )
 }
 
