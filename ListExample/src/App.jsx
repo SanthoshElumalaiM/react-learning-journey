cf5e3cf2c56e86fee3004d5f045ce0b6963802fd
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-04 17:09:28 +0530

    listAndKey

---
+++ ListExample/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListAndKey from './Components/ListAndKey'
+
+const App = () => {
+  return (
+    <div>
+        <ListAndKey/>
+    </div>
+  )
+}
+
+export default App
---
+++ ListExample/src/Components/ListAndKey.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const ListAndKey = () => {
+    let subject=["html","css","java","J2EE"]
+    let product=[
+        {
+            id:1,
+            productName:"Laptop",
+            price:4999
+        },
+        {
+            id:2,
+            productName:"mobile",
+            price:41209
+        },
+        {
+            id:3,
+            productName:"Laptop",
+            price:4999
+        },
+    ]
+  return (
+    <div>
+        <h1>List And Key</h1>
+        <ol>
+            {
+                subject.map((ele,index)=>{
+                    return(
+                        <li key={index}>{ele}</li>
+                    )
+                })
+            }
+        </ol>
+        <div className="div">
+            
+                {
+                    product.map((ele)=>{
+                        return(<div className="box" key={ele.id}>
+                            <h3>productName:{ele.productName}</h3>
+                            <h3>productName:{ele.price}</h3>
+                        </div>
+                            
+                        )
+                    })
+                }
+           
+        </div>
+    </div>
+  )
+}
+
+export default ListAndKey
