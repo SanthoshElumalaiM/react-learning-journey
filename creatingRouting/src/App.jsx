2a8a204097a5d70c06adc3234c8f6ceac5f171bb
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-06 17:43:41 +0530

    app

--- creatingRouting/src/App.jsx
+++ creatingRouting/src/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './Components/Home'
 import About from './Components/About'
 import ContactUs from './Components/ContactUs'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import NavBar from './Components/NavBar'
 
 const App = () => {
   return (
     <>
-    
     <BrowserRouter>
-      <NavBar/>
+    <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
-        <Route path='/contact' element={<ContactUs/>} ></Route>
+        <Route path='/contact' element={<ContactUs/>}></Route>
       </Routes>
-    
     </BrowserRouter>
-
     </>
   )
 }
